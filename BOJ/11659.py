8d3dc3f50945d96909b040b13350e924aa8e7c11
weidon <jweidon0821@gmail.com>
2022-08-31 23:39:51 +0900

    11659 Correct

--- BOJ/11659.py
+++ BOJ/11659.py
@@ -2,9 +2,16 @@ import sys; input = sys.stdin.readline
 
 N, M = map(int, input().split())
 nums = list(map(int, input().split()))
+
+prefix = [0]
+temp = 0
+for n in nums:
+    temp += n
+    prefix.append(temp)
+
 result = []
 for _ in range(M):
     i, j = map(int, input().split())
-    result.append(sum(nums[i-1:j]))
+    result.append(prefix[j]-prefix[i-1])
 
 print("\n".join(map(str, result)))
