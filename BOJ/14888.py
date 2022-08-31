03fd7448db29463930b364c38f8eeaa3be7d354f
weidon <jweidon0821@gmail.com>
2022-08-31 22:27:28 +0900

    14888 Wrong: timeout

---
+++ BOJ/14888.py
@@ -0,0 +1,27 @@
+from itertools import permutations
+
+N = int(input())
+A = list(map(int, input().split()))
+C = list(map(int, input().split()))
+cal = []
+for i in range(4):
+    for _ in range(C[i]):
+        cal.append(i)
+permute = list(permutations(cal))
+
+result = []
+for express in permute:
+    num = A[0]
+    for i in range(1, N):
+        if express[i-1] == 0:
+            num += A[i]
+        elif express[i-1] == 1:
+            num -= A[i]
+        elif express[i-1] == 2:
+            num *= A[i]
+        elif express[i-1] == 3:
+            num //= A[i]
+    result.append(num)
+
+print(max(result))
+print(min(result))
