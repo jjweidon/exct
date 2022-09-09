4014e1df1774093652d7b046d447922d4a542084
weidon <jweidon0821@gmail.com>
2022-09-10 00:02:46 +0900

    1300 Wrong: Output Limit Exceed

---
+++ BOJ/1300.py
@@ -0,0 +1,23 @@
+N = int(input())
+k = int(input())
+
+def binarySearch(N, k):
+    start = 1
+    end = min(1e9, N*N)
+    answer = 0
+
+    while start <= end:
+        mid = (start + end) // 2
+        cnt = 0
+        for i in range(1, N+1):
+            cnt += min(mid // i, N)
+        print(f'start: {start}, end: {end}, mid: {mid}, cnt: {cnt}')
+
+        if cnt >= k:
+            answer = mid
+            end = mid - 1
+        elif cnt > k:
+            start = mid + 1
+    return answer
+
+print(binarySearch(N, k))
