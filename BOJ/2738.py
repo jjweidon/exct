3469d310d22067363828b0e4f877f7664d5b1713
weidon <jweidon0821@gmail.com>
2022-12-28 10:04:35 +0900

    2738 Correct 2: list comprehension

--- BOJ/2738.py
+++ BOJ/2738.py
@@ -2,12 +2,8 @@ import sys
 input = sys.stdin.readline
 
 N, M = map(int, input().split())
-arr_A = []
-arr_B = []
-for _ in range(N):
-    arr_A.append(list(map(int, input().split())))
-for _ in range(N):
-    arr_B.append(list(map(int, input().split())))
+arr_A = [list(map(int, input().split())) for _ in range(N)]
+arr_B = [list(map(int, input().split())) for _ in range(N)]
 
 for i in range(N):
     for j in range(M):
