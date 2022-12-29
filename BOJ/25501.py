2b853c0802242a0e3f2b6376826a59963abd2ae5
weidon <jweidon0821@gmail.com>
2022-12-30 01:42:43 +0900

    25501 Correct

---
+++ BOJ/25501.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+def recursion(s, l, r, cnt):
+    cnt += 1
+    if l >= r: return 1, cnt
+    elif s[l] != s[r]: return 0, cnt
+    else: return recursion(s, l+1, r-1, cnt)
+
+def isPalindrome(s):
+    return recursion(s, 0, len(s)-1, cnt)
+
+T = int(input())    
+for _ in range(T):
+    S = input().strip()
+    cnt = 0 
+    pal, cnt = isPalindrome(S)
+    print(pal, cnt)
