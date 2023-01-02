fca7adc1e2fd7bf7a81301056a9ac6ee26cd3422
weidon <jweidon0821@gmail.com>
2023-01-02 20:15:28 +0900

    24416 Wrong 2: timeout

--- BOJ/24416.py
+++ BOJ/24416.py
@@ -1,24 +1,19 @@
-cnt1 = 0
-cnt2 = 0
-
-def fib(n):
-    global cnt1
+def fib(n, cnt): 
     if n == 1 or n == 2:
-        cnt1 += 1
-        return 1
+        cnt += 1
+        return 1, cnt
     else:
-        return (fib(n - 1) + fib(n - 2))
+        return (fib(n - 1, cnt)[0] + fib(n - 2, cnt)[0], cnt)
 
 def fibonacci(n):
-    global cnt2
+    cnt2 = 0
     f = [0, 1, 1]
     for i in range(3, n + 1):
         f.append(f[i - 1] + f[i - 2])
         cnt2 += 1
-    return f[n]
+    return cnt2
 
 n = int(input())
-fib(n)
-fibonacci(n)
+cnt1 = 0
 
-print(cnt1, cnt2)
+print(fib(n, cnt1)[0], fibonacci(n))
