2b5b71086292268a476179a9395a02d0bf7bb5ed
weidon <jweidon0821@gmail.com>
2023-01-04 01:12:37 +0900

    1904 Correct

--- BOJ/1904.py
+++ BOJ/1904.py
@@ -11,14 +11,13 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-
 tile = [0] * (N + 1)
-
 tile[1] = 1
-tile[2] = 2
 
-for i in range(3, N + 1):
-    tile[i] = tile[i - 1] + tile[i - 2]
-    if tile[i] >= 15746: tile[i] = tile[i] % 15746
+if N != 1:
+    tile[2] = 2
+    for i in range(3, N + 1):
+        tile[i] = tile[i - 1] + tile[i - 2]
+        if tile[i] >= 15746: tile[i] = tile[i] % 15746
 
 print(tile[N])
