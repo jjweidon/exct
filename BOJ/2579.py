be053a9a65e80e652d23fd51dd9e09a155888096
weidon <jweidon0821@gmail.com>
2023-01-19 22:05:35 +0900

    2579 Correct

--- BOJ/2579.py
+++ BOJ/2579.py
@@ -1,40 +1,17 @@
-"""
-1. 아이디어
-- 마지막 인덱스부터 시작해서 연속 한개일 때: 둘 중 더 큰수로 뛰기 / 연속 두개일 때: 두칸 뛰기
-2. 시간복잡도
-- O(N/2)
-3. 자료구조
-- stair: int[] / step: bool
-"""
-
 import sys
 input = sys.stdin.readline
 
 N = int(input())
-stair = [0] * N
-for i in range(N):
-    stair[i] = int(input())
+stair = [int(input()) for _ in range(N)]
+dp = [0] * N
 
-step = False
-index = N - 1
-rs = stair[index]
-while index >= 2:
-    if step:
-        index -= 2
-        step = False
-        
-    else:
-        if stair[index-1] >= stair[index-2]:
-            index -= 1
-            step = True
-        else:
-            index -= 2
-            # step = False
-    
-    rs += stair[index]
+dp[0] = stair[0]
+if N >= 2:
+    dp[1] = stair[1] + stair[0]
+    if N >= 3:
+        dp[2] = stair[2] + max(stair[0], stair[1])
 
-# index error를 회피하기 위해 index = 2까지 while 돌리고 그 이하는 따로 처리
-if index and not step:
-    rs += stair[0]
+for i in range(3, N):
+    dp[i] = max(dp[i-2], dp[i-3] + stair[i-1]) + stair[i]
 
-print(rs)
+print(dp[N - 1])
