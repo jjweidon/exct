41995aa2dcd90ca384c846b1dfa4d8b24d469bc2
weidon <jweidon0821@gmail.com>
2023-01-31 19:55:14 +0900

    4949 Wrong

---
+++ BOJ/4949.py
@@ -0,0 +1,28 @@
+import sys
+input = sys.stdin.readline
+
+while 1:
+    sentence = input().rstrip()
+    if sentence == '.':
+        break
+
+    stack = []
+    for x in sentence:
+        if x == '(':
+            stack.append(x)
+        elif x == '[':
+            stack.append(x)
+        elif x == ')':
+            if stack and stack[-1] == '(':
+                stack.pop()
+            else:
+                print('no')
+                break
+        elif x == ']':
+            if stack and stack[-1] == '[':
+                stack.pop()
+            else:
+                print('no')
+                break
+    else:
+        print('yes')
