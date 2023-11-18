5ef26062c7b4ecaeb0d44cab98d6f06075df3069
정재웅 <jweidon0821@gmail.com>
2023-11-18 13:09:15 +0900

    wrong 1(시간초과): 미로 탎출

---
+++ programmers/미로_탈출.py
@@ -0,0 +1,57 @@
+from collections import deque
+import sys
+sys.setrecursionlimit(1000000)
+
+def solution(maps):
+    H = len(maps)
+    W = len(maps[0])
+    answer = 0
+    start = [0, 0]
+    end = [0, 0]
+    lever = [0, 0]
+    
+    start_to_lever = 0
+    lever_to_end = 0
+    
+    # 시작점, 출구, 레버 찾기
+    for j in range(H):
+        for i in range(W):
+            if maps[j][i] == 'S':
+                start = [j, i]
+            elif maps[j][i] == 'E':
+                end = [j, i]
+            elif maps[j][i] == 'L':
+                lever = [j, i]
+    
+    dy = [1, -1, 0, 0]
+    dx = [0, 0, 1, -1]
+    visited = [[False] * W for _ in range(H)]
+    
+    def check_time(s, e, step):
+        q = deque([s])
+        visited[s[0]][s[1]] = True
+        next = []
+        while q:
+            while q:
+                ey, ex = q.popleft()
+                if ey == e[0] and ex == e[1]:
+                    return step
+                for k in range(4):
+                    ny = ey + dy[k]
+                    nx = ex + dx[k]
+                    if ny in (-1, H) or nx in (-1, W):
+                        continue
+                    if maps[ny][nx] == 'X' or visited[ny][nx] == True:
+                        continue
+                    next.append((ny, nx))
+            q.extend(next)
+            next.clear()
+            step += 1
+        return 0
+    
+    start_to_lever = check_time(start, lever, 0)
+    if start_to_lever:
+        lever_to_end = check_time(lever, end, 0)
+    answer = start_to_lever + lever_to_end
+    
+    return answer if answer else -1
