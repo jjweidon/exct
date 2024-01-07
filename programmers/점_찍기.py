7788c6b119b882628531c8b536e6ba4fa95fba40
정재웅 <jweidon0821@gmail.com>
2024-01-08 02:08:08 +0900

    correct: 점 찍기

---
+++ programmers/점_찍기.py
@@ -0,0 +1,9 @@
+import math
+
+def solution(k, d):    
+    answer = 0
+    
+    for i in range(0, d+1, k):
+        answer += int(math.sqrt(pow(d, 2) - pow(i, 2))) // k + 1
+        
+    return answer
