58fe9451cf41bc14d1001683e06e9dfe724dead0
정재웅 <jweidon0821@gmail.com>
2024-01-10 04:33:01 +0900

    correct: 인사고과

--- programmers/인사고과.py
+++ programmers/인사고과.py
@@ -3,23 +3,28 @@ def solution(scores):
     scores.sort(key=lambda x: (-x[0], -x[1]))
     insentive = [scores[0]]
     limit_x, limit_y = scores[0][0], scores[0][1]
-    for i in range(1, len(scores)):
+    temp_x, temp_y = limit_x, limit_y
+
+    for i in range(1, len(scores)):      
         x, y = scores[i][0], scores[i][1]
+        if x < temp_x:
+            limit_x = temp_x
+            limit_y = temp_y
+            temp_x = x
+            if y > limit_y:
+                temp_y = y
+        
         if x < limit_x and y < limit_y:
             continue
-        if y > limit_y:
-            limit_y = y
-        if x < limit_x:
-            limit_x = insentive[-1][0]
         insentive.append(scores[i])
-    
+        
     insentive.sort(key=lambda x: -(x[0]+x[1]))
     
     rank = 1
-    maxx = insentive[0][0] + insentive[0][1]
+    maxx = sum(insentive[0])
     for i in range(len(insentive)):
-        if insentive[i][0] + insentive[i][1] < maxx:
-            maxx = insentive[i][0] + insentive[i][1]
+        if sum(insentive[i]) < maxx:
+            maxx = sum(insentive[i])
             rank = i+1        
         if insentive[i] == wanho:
             return rank
