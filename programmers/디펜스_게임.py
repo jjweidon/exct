684e52b049f7721aa13f07713f9c416bf2d7e769
정재웅 <jweidon0821@gmail.com>
2024-01-17 20:13:35 +0900

    correct: 디펜스 게임

--- programmers/디펜스_게임.py
+++ programmers/디펜스_게임.py
@@ -1,27 +1,11 @@
-import sys
-sys.setrecursionlimit(100000)
+import heapq
 
 def solution(n, k, enemy):
-    N = len(enemy)
-    dp = [[0] * (k+1) for _ in range(N)]
-    visited = [[False] * (k+1) for _ in range(N)]
-    dp[0][0] = enemy[0]
-    for j in range(1, N):
-        dp[j][0] = dp[j-1][0] + enemy[j]
-        visited[j][0] = True
-    for i in range(k+1):
-        visited[0][i] = True
-    
-    def dfs(j, i):
-        if not visited[j][i]:
-            dp[j][i] = min(dfs(j-1, i-1), dfs(j-1, i) + enemy[j])
-            visited[j][i] = True
-        return dp[j][i]
-    
-    for j in range(1, N):
-        dp[j][k] = dfs(j, k)
-        if dp[j][k] == n:
-            return j+1
-        elif dp[j][k] > n:
-            return j
-    return N
+    heap = []
+    for i, e in enumerate(enemy):
+        heapq.heappush(heap, e)
+        if len(heap) > k:
+            n -= heapq.heappop(heap)
+        if n < 0:
+            return i
+    return len(enemy)
