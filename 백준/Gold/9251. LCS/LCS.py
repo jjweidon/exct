034ecedbcbef52cb97297e31aa18c6725641ca83
정재웅 <112147526+jjweidon@users.noreply.github.com>
2024-09-03 17:45:08 +0900

    [Gold V] Title: LCS, Time: 452 ms, Memory: 55708 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -1,20 +1,26 @@
-str1 = list(input())
-str2 = list(input())
-N, M = len(str1), len(str2)
-dp = [[0] * N for _ in range(M)]
+str1 = input()
+str2 = input()
+
+dp = [[0] * len(str1) for _ in range(len(str2))]
+
+# 초기화
 if str1[0] == str2[0]:
     dp[0][0] = 1
 
-for j in range(1, M):
-    if str2[j] == str1[0] or dp[j-1][0]:
-        dp[j][0] = 1
-
-for i in range(1, N):
-    if str1[i] == str2[0] or dp[0][i-1]:
+for i in range(1, len(str1)):
+    if dp[0][i-1] or str1[i] == str2[0]:
         dp[0][i] = 1
 
-for j in range(1, M):
-    for i in range(1, N):
-        dp[j][i] = dp[j-1][i-1] + 1 if str2[j] == str1[i] else max(dp[j][i-1], dp[j-1][i])
-        
-print(dp[M-1][N-1])
+for j in range(1, len(str2)):
+    if dp[j-1][0] or str2[j] == str1[0]:
+        dp[j][0] = 1
+
+# dp 테이블 채우기
+for j in range(1, len(str2)):
+    for i in range(1, len(str1)):
+        if str1[i] == str2[j]:
+            dp[j][i] = dp[j-1][i-1] + 1
+        else:
+            dp[j][i] = max(dp[j-1][i], dp[j][i-1])
+
+print(dp[-1][-1])
