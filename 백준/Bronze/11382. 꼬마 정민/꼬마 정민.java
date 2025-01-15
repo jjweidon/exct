a15aa3b321af89cb5b0810eed58eba1daf7214d8
정재웅 <112147526+jjweidon@users.noreply.github.com>
2025-01-15 19:25:31 +0900

    [Bronze V] Title: 꼬마 정민, Time: 120 ms, Memory: 15868 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        br.close();
+        
+        long A = Long.parseLong(st.nextToken());
+        long B = Long.parseLong(st.nextToken());
+        long C = Long.parseLong(st.nextToken());
+        
+        bw.write((A + B + C) + "\n");
+        
+        bw.flush();
+        bw.close();
+    }
+}
